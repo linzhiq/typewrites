d3fda955dca4b14208059b33995af684419ceec2
Qi Linzhi <qi@qlz.io>
2021-09-06 17:24:33 -0700

    chore: skeleton of api in ts

--- api/src/index.ts
+++ api/src/index.ts
@@ -1,17 +1,18 @@
-import express from 'express';
-import path from 'path';
+import express from "express";
+import path from "path";
 
 const app = express();
 app.use(express.json());
-app.use(express.urlencoded({extended:false}));
-process.env.NODE_ENV === 'development' && app.use(express.static(path.join(__dirname, '../web/dist')));
+app.use(express.urlencoded({ extended: false }));
+process.env.NODE_ENV === "development" &&
+  app.use(express.static(path.join(__dirname, "../web/dist")));
 
-app.get('/', (req, res) => {
-  const name = process.env.NAME || 'World';
+app.get("/", (req, res) => {
+  const name = process.env.NAME || "World";
   res.send(`Hello ${name}!`);
 });
 
 const PORT = process.env.PORT || 8080;
-app.listen(PORT, ()=>{
-  console.log(`Server running on PORT ${ PORT }`);
-})
+app.listen(PORT, () => {
+  console.log(`Server running on PORT ${PORT}`);
+});
